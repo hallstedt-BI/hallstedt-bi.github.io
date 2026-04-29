c6bd146c29e70729c4c4e4e6cf1ba4639a04c49e
Amanda Hallstedt <amanda.hallstedt@boardintelligence.com>
2026-04-29 10:09:07 +0200

    slutspels-push

---
+++ fifa/firebase-config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDwacTQur6u1qkjtqhpEe3HBBupK0duiZU",
+    authDomain: "teamenginefifa.firebaseapp.com",
+    databaseURL: "https://teamenginefifa-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "teamenginefifa",
+    storageBucket: "teamenginefifa.firebasestorage.app",
+    messagingSenderId: "56750511073",
+    appId: "1:56750511073:web:9c6de40497a4beca0138a0"
+};
